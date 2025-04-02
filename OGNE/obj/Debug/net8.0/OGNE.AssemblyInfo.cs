d21cad157f433a79b1eec36617507442e2f36f65
Gabriel Iuza Frost <109495931+gabroisreal@users.noreply.github.com>
2025-04-02 09:45:09 -0500

    testing

--- OGNE/obj/Debug/net8.0/OGNE.AssemblyInfo.cs
+++ OGNE/obj/Debug/net8.0/OGNE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGNE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a25ba041a312ddb2f61a368cb72bce2e15de500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdfe6cb4451d765db34b0a057f2bb6d45ed8c824")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGNE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGNE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
