0ae62a4b8cb6d5149c769d49f1b40f793ed13145
Gabriel Iuza Frost <109495931+gabroisreal@users.noreply.github.com>
2025-04-08 10:27:33 -0500

    added unique users

--- OGNE/obj/Debug/net8.0/OGNE.AssemblyInfo.cs
+++ OGNE/obj/Debug/net8.0/OGNE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGNE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae07bcc3779e9bf6a824062891af1a2f6a8c2a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2593bf49084c7420bccab0a1b8e1343655c618c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGNE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGNE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
